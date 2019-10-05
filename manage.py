2512299970e09e813a363a5024a8ef20f640e170
Ubuntu <ubuntu@ip-172-31-50-137.us-west-2.compute.internal>
2019-10-05 23:52:43 +0000

    Fix: project naming conflict

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beavergo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeaverGo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
