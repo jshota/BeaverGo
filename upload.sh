38f72207eea40dc4cf0abe1fa6d0581634d53dc5
HaoYC <chengyua@oregonstate.edu>
2019-10-07 15:09:36 -0700

    Fix: upload.sh

--- upload.sh
+++ upload.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-LightGreen='\033[1;32m'
+Green='\033[0;32m'
 White='\033[1;37m'
-echo -e "${White}Update Progress..."
-echo -e "${White}================="
+NC='\033[0m'
+echo -e "${White}Update Progress...${NC}"
+echo -e "${White}=================${NC}"
 git add .
 echo -n "add commit to the repo: "
 read input
 
 
 git commit -m "$input"
-echo -e "${LightGreen}git commit -m \"$input\" --Done"
+echo -e "${Green}git commit -m \"$input\" --Done${NC}"
 git checkout master
-echo -e "${LightGreen}git git checkout master --Done"
+echo -e "${Green}git git checkout master --Done${NC}"
 git merge dev
-echo -e "${LightGreen}git merge dev --Done"
+echo -e "${Green}git merge dev --Done${NC}"
 git push origin master
-echo -e "${LightGreen}git push origin master --Done"
+echo -e "${Green}git push origin master --Done${NC}"
 git checkout dev
-echo -e "${LightGreen}git checkout dev --Done"
+echo -e "${Green}git checkout dev --Done${NC}"
 git push origin dev
-echo -e "${LightGreen}git push origin dev --Done"
-echo -e "${White}================="
-echo -e "${White}Done"
+echo -e "${Green}git push origin dev --Done${NC}"
+echo -e "${White}=================${NC}"
+echo -e "${White}Done${NC}"
