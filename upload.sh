858e19621b3376ee733aae2d6dedb923d7dc2d23
HaoYC <chengyua@oregonstate.edu>
2019-10-07 14:54:35 -0700

    Fix: upload.sh in master is up to date but dev is not

--- upload.sh
+++ upload.sh
@@ -5,10 +5,18 @@ git add .
 echo 'add commit to the repo:'
 read input
 
+
 git commit -m "$input"
+echo 'git commit -m "$input" --Done'
 git checkout master
+echo 'git git checkout master --Done'
 git merge dev
+echo 'git merge dev --Done'
 git push origin master
+echo 'git push origin master --Done'
 git checkout dev
+echo 'git checkout dev --Done'
+git push origin dev
+echo 'git push origin dev --Done'
 echo '================='
 echo 'Done'
