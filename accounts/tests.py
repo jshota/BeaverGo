89df32c5efc7846fc1c37007c6fbc8e8ecd79d20
jshota <jshota@hotmail.com>
2019-10-30 15:04:09 -0700

    Add: Sign-up and Profile test

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,10 +5,12 @@ from accounts.models import CustomUser
 import json
 
 class TestUrls(TestCase):
+    # Sign-up URL test
     def test_signup_url_resolved(self):
         url = reverse('signup')
         self.assertEquals(resolve(url).func.view_class, SignUp)
     
+    # Profile URL test
     def test_profile_url_resolved(self):
         url = reverse('profile_view')
         self.assertEquals(resolve(url).func, profile_view)
@@ -18,23 +20,30 @@ class TestViews(TestCase):
         self.client = Client()
         self.signup_url = reverse('signup')
         self.profile_url = reverse('profile_view')
+        CustomUser.objects.create_user(
+            username = 'Jane Test',
+            password = '23sdaf235@',
+            gender = 'Male',
+            address = 'fdsafds',
+            ssn = '123456', 
+            email = 'test@test.com')
 
+    # Sign-up UI test
     def test_signup_GET(self):
         response = self.client.get(self.signup_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup.html')
 
+    # User-Profile UI test
     def test_profile_GET(self):
         response = self.client.get(self.profile_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/profile.html')
 
-    def test_profile_input(self):
-
-        response = self.client.post(self.profile_url, {
-            'username': 'test',
-            'email': 'test@test.com',
-            'password': '^ERTET122#@$'
-        })
-
-        self.assertEquals(response.status_code, 200)
+    # Sign-up test
+    def test_signup_signin(self):
+        test_user = CustomUser.objects.get(username = 'Jane Test')
+        self.assertEquals(test_user.gender, 'Male')
+        self.assertEquals(test_user.address, 'fdsafds')
+        self.assertEquals(test_user.ssn, '123456')
+        self.assertEquals(test_user.email, 'test@test.com')
